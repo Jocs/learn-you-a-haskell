8e86d9e26d2742f872dbe75c7754b6238987829b
罗冉 <luoran1988@126.com>
2016-02-29 09:24:10 +0800

    d

--- chapter15/tree.hs
+++ chapter15/tree.hs
@@ -24,14 +24,16 @@ data Direction = L | R deriving (Show)
 type Directions = [Direction]
 
 changeToP :: Directions -> Tree Char -> Tree Char
+changeToP [] (Node _ l r) = Node 'P' l r
 changeToP (L:ds) (Node x l r) = Node x (changeToP ds l) r
 changeToP (R:ds) (Node x l r) = Node x l (changeToP ds r)
-changeToP [] (Node _ l r) = Node 'P' l r
 
 elemAt :: Directions -> Tree a -> a
 elemAt (L:ds) (Node _ l _) = elemAt ds l
 elemAt (R:ds) (Node _ _ r) = elemAt ds r
 elemAt [] (Node x _ _) = x
+
+tree = foldr treeInsert EmptyTree ['O', 'B', 'D', 'E']
 
 
 
